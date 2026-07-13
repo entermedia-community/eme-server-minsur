c06dd3131d93a2dacd548b8a620384e10e6cc1f9
Cristobal Mejia <cristobal.mejia@gmail.com>
2026-07-13 15:01:54 -0600

    less verbose compiling

--- plugins/system/resources/bin/compile.sh
+++ plugins/system/resources/bin/compile.sh
@@ -3,10 +3,12 @@
 mkdir -p bin && \
 find plugins/system/code plugins/openedit/code \
 	-type f \( -name '*.xml' -o -name '*.properties' \) \
-	-exec cp --parents {} bin \; && \
-javac -verbose -g -d bin \
+	-exec cp --parents {} bin \;
+echo "Compiling Java code..." 
+javac -g -d bin \
 	--source 21 --target 21 -nowarn \
 	-classpath "$(find plugins/system/lib plugins/finder/lib plugins/community/lib \
 		-type f \( -name '*.jar' -o -path '*/compile/*.jar' \) | tr '\n' ':')" \
 	$(find plugins/openedit/code plugins/system/code plugins/finder/code plugins/community/code \
 		-type f -name '*.java')
+echo "Compiling Java finished." 
