c00a17f3a6165ed3419c4db97bd967a948ca17a2
Cristobal Mejia <cristobal.mejia@gmail.com>
2026-07-14 17:37:42 -0600

    Move Community to independent project: eme-plugin-community

--- plugins/community/code/org/entermedia/transactions/TransactionStatus.java
+++
@@ -1,9 +0,0 @@
-package org.entermedia.transactions;
-
-public class TransactionStatus
-{
-	public static String PAID = "paid";
-	public static String UNPAID = "unpaid";
-	public static String CANCELED = "canceled";
-	public static String REFUNDED = "refunded";
-}
